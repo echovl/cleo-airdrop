4e1bc1d04ec4da1542ace44901ab5a0db75b9cb5
Alonso Villegas <alonsovillegas816@gmail.com>
2023-07-25 17:11:10 -0500

    web3 sucks

--- snapshot.py
+++ snapshot.py
@@ -1,32 +1,83 @@
 import os
+from typing import List
 
+from multicall import Call, Multicall
 from web3 import Web3, contract
 
+VERAM_LOCKED_ABI = "locked(uint256)(uint256,uint256)"
+VERAM_OWNEROF_ABI = "ownerOf(uint256)(address)"
+ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
+MIN_LOCKED_AMOUNT = 100_000 * 1e18
 
-def get_potential_holders(w3: Web3, venft: contract.Contract) -> [str]:
-    genesis_block = 69820005
-    latest_block = w3.eth.block_number
 
-    venft_depositors = []
-    current_block = genesis_block
-    block_step = 5000000
+class VeToken:
+    def __init__(
+        self, token_id: int, owner: str, locked_amount: int, lock_end: int
+    ) -> None:
+        self.token_id = token_id
+        self.owner = owner
+        self.locked_amount = locked_amount
+        self.lock_end = lock_end
 
-    while current_block < latest_block:
-        from_block = current_block
-        to_block = from_block + block_step
 
-        if to_block > latest_block:
-            to_block = latest_block
+def batch_multicall(calls: List[Call], batch_size: int):
+    step = int(len(calls) / batch_size)
 
-        event_filter = venft.events.Deposit.create_filter(
-            fromBlock=from_block, toBlock=to_block
+    output = {}
+    for i in range(0, len(calls), step):
+        print(f"chunk {i}")
+        res = Multicall(calls[i : i + step])()
+        output = {**output, **res}
+
+    return output
+
+
+def get_eligible_tokens(w3: Web3, venft: contract.Contract) -> List[VeToken]:
+    max_token_id = venft.functions.tokenId().call()
+
+    os.environ["AIOHTTP_TIMEOUT"] = "300"
+
+    token_ids = range(max_token_id)
+
+    locked_calls = [
+        Call(
+            venft.address,
+            [VERAM_LOCKED_ABI, id],
+            [(str(id) + "_amount", None), (str(id) + "_end", None)],
         )
+        for id in token_ids
+    ]
 
-        events = event_filter.get_new_entries()
-        venft_depositors += map(lambda e: e.args.provider, events)
-        current_block = to_block + 1
+    ownerof_calls = [
+        Call(venft.address, [VERAM_OWNEROF_ABI, id], [(str(id), None)])
+        for id in token_ids
+    ]
 
-    return list(set(venft_depositors))
+    locked = batch_multicall(locked_calls, 30)
+    ownerof = batch_multicall(ownerof_calls, 30)
+
+    ve_tokens: List[VeToken] = []
+
+    for token_id in token_ids:
+        owner: str = ownerof.get(str(token_id))
+        locked_amount: int = locked.get(str(token_id) + "_amount")
+        lock_end: int = locked.get(str(token_id) + "_end")
+
+        if owner == None and owner != ZERO_ADDRESS:
+            continue
+
+        if locked_amount == None:
+            continue
+
+        if locked_amount < MIN_LOCKED_AMOUNT:
+            continue
+
+        if owner.lower() == "0xcba1a275e2d858ecffaf7a87f606f74b719a8a93":
+            print(token_id, locked_amount, lock_end)
+
+        ve_tokens.append(VeToken(token_id, owner.lower(), locked_amount, lock_end))
+
+    return ve_tokens
 
 
 rpc_url = os.environ.get("RPC_URL")
@@ -37,13 +88,13 @@ if rpc_url == None:
 
 w3 = Web3(Web3.HTTPProvider(rpc_url))
 
-with open("./abi//veNFT.json") as abi:
+with open("./abi/veNFT.json") as abi:
     venft_abi = abi.read()
 
 venft = w3.eth.contract(
     address="0xAAA343032aA79eE9a6897Dab03bef967c3289a06", abi=venft_abi
 )
 
-depositors = get_potential_holders(w3, venft)
+tokens = get_eligible_tokens(w3, venft)
 
-print(len(depositors))
+print(len(tokens))
