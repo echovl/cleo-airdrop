ff4ae6165c29840bec1d1d5c58bd1c183521029b
Alonso Villegas <alonsovillegas816@gmail.com>
2023-07-25 22:19:21 -0500

    first version

---
+++ constants.py
@@ -0,0 +1,51 @@
+VERAM_ADDRESS = "0xAAA343032aA79eE9a6897Dab03bef967c3289a06"
+ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
+
+VERAM_TOKEN_ID = "tokenId()(uint256)"
+VERAM_LOCKED_ABI = "locked(uint256)(uint256,uint256)"
+VERAM_OWNEROF_ABI = "ownerOf(uint256)(address)"
+
+MIN_LOCKED_AMOUNT = 100_000
+AIRDROP_ALLOCATION = 10_000
+THREE_AND_HALF_YEARS_IN_SECONDS = 3.5 * 365 * 24 * 60 * 60
+
+
+BLACKLISTED_NFTS = [
+    1,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    13,
+    14,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    30,
+    31,
+    232,
+    266,
+    322,
+    1119,
+    1407,
+    1911,
+    1987,
+    2324,
+    2444,
+    2563,
+    2564,
+    2569,
+    2745,
+    2773,
+    2785,
+    2834,
+    2884,
+]
--- snapshot.py
+++ snapshot.py
@@ -1,23 +1,32 @@
+import csv
 import os
+import time
 from typing import List
 
 from multicall import Call, Multicall
-from web3 import Web3, contract
+from web3 import Web3
 
-VERAM_LOCKED_ABI = "locked(uint256)(uint256,uint256)"
-VERAM_OWNEROF_ABI = "ownerOf(uint256)(address)"
-ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
-MIN_LOCKED_AMOUNT = 100_000 * 1e18
+import constants
 
 
 class VeToken:
     def __init__(
-        self, token_id: int, owner: str, locked_amount: int, lock_end: int
+        self,
+        token_id: int,
+        owner: str,
+        locked_amount: int,
+        lock_end: int,
+        airdrop_amount: int = 0,
     ) -> None:
         self.token_id = token_id
         self.owner = owner
         self.locked_amount = locked_amount
         self.lock_end = lock_end
+        self.airdrop_amount = airdrop_amount
+
+
+def from_wei(value) -> int:
+    return value / 1e18
 
 
 def batch_multicall(w3: Web3, calls: List[Call], batch_size: int):
@@ -32,24 +41,33 @@ def batch_multicall(w3: Web3, calls: List[Call], batch_size: int):
     return output
 
 
-def get_eligible_tokens(w3: Web3, venft: contract.Contract) -> List[VeToken]:
-    max_token_id = venft.functions.tokenId().call()
+def get_eligible_tokens(w3: Web3) -> List[VeToken]:
+    max_token_id = Call(constants.VERAM_ADDRESS, [constants.VERAM_TOKEN_ID], _w3=w3)()
 
+    print(max_token_id)
+
+    timestamp = time.time()
     os.environ["AIOHTTP_TIMEOUT"] = "300"
 
-    token_ids = range(max_token_id)
+    token_ids = [
+        id for id in range(max_token_id) if id not in constants.BLACKLISTED_NFTS
+    ]
 
     locked_calls = [
         Call(
-            venft.address,
-            [VERAM_LOCKED_ABI, id],
-            [(str(id) + "_amount", None), (str(id) + "_end", None)],
+            constants.VERAM_ADDRESS,
+            [constants.VERAM_LOCKED_ABI, id],
+            [(str(id) + "_amount", from_wei), (str(id) + "_end", None)],
         )
         for id in token_ids
     ]
 
     ownerof_calls = [
-        Call(venft.address, [VERAM_OWNEROF_ABI, id], [(str(id), None)])
+        Call(
+            constants.VERAM_ADDRESS,
+            [constants.VERAM_OWNEROF_ABI, id],
+            [(str(id), None)],
+        )
         for id in token_ids
     ]
 
@@ -62,35 +80,60 @@ def get_eligible_tokens(w3: Web3, venft: contract.Contract) -> List[VeToken]:
         owner: str = ownerof.get(str(token_id))
         locked_amount: int = locked.get(str(token_id) + "_amount")
         lock_end: int = locked.get(str(token_id) + "_end")
+        locked_time = lock_end - timestamp
 
-        if owner == None and owner != ZERO_ADDRESS:
+        if owner == None or owner == constants.ZERO_ADDRESS:
             continue
 
-        if locked_amount == None:
+        if locked_amount == None or locked_amount < constants.MIN_LOCKED_AMOUNT:
             continue
 
-        if locked_amount < MIN_LOCKED_AMOUNT:
+        if locked_amount < constants.MIN_LOCKED_AMOUNT:
+            continue
+
+        if locked_time < constants.THREE_AND_HALF_YEARS_IN_SECONDS:
             continue
 
         ve_tokens.append(VeToken(token_id, owner.lower(), locked_amount, lock_end))
 
+    total_ve = sum(token.locked_amount for token in ve_tokens)
+    for token in ve_tokens:
+        token.airdrop_amount = (
+            constants.AIRDROP_ALLOCATION * token.locked_amount / total_ve
+        )
+
+    assert constants.AIRDROP_ALLOCATION == sum(
+        token.airdrop_amount for token in ve_tokens
+    ), f"Calculated airdrop total amount must equal {constants.AIRDROP_ALLOCATION}"
+
     return ve_tokens
 
 
-rpc_url = os.environ.get("RPC_URL")
+def take_snapshot():
+    rpc_url = os.environ.get("RPC_URL")
+    if rpc_url == None:
+        rpc_url = "https://arb1.arbitrum.io/rpc"
 
-if rpc_url == None:
-    rpc_url = "https://arb1.arbitrum.io/rpc"
+    w3 = Web3(Web3.HTTPProvider(rpc_url))
 
-w3 = Web3(Web3.HTTPProvider(rpc_url))
+    tokens = get_eligible_tokens(w3)
+    tokens = sorted(tokens, key=lambda t: t.locked_amount, reverse=True)
 
-with open("./abi/veNFT.json") as abi:
-    venft_abi = abi.read()
+    with open("airdrop.csv", "w", newline="") as csvfile:
+        fieldnames = ["token_id", "owner", "balance", "allocation"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-venft = w3.eth.contract(
-    address="0xAAA343032aA79eE9a6897Dab03bef967c3289a06", abi=venft_abi
-)
+        writer.writeheader()
 
-tokens = get_eligible_tokens(w3, venft)
+        for tk in tokens:
+            writer.writerow(
+                {
+                    "token_id": tk.token_id,
+                    "owner": tk.owner,
+                    "balance": tk.locked_amount,
+                    "allocation": tk.airdrop_amount,
+                }
+            )
 
-print(len(tokens))
+
+take_snapshot()
